8782e142bc6aecbfa973bfbd42f43f7a1931e119
Junhyeok <applestrawberryg@gmail.com>
2022-07-16 01:07:15 +0900

    feat: add validation token & get user id from token

--- module/member_module.py
+++ module/member_module.py
@@ -156,4 +156,87 @@ def find_password(db):
         print(ex)
         print('*********')
         return False
-    
+
+"""
+* 토큰 유효성 검사
+* 로그인 유효성 검사가 필요한 함수 위에 @login_required를 넣으면 됨
+*   예시)    @app.route('/action', methods = ["POST"])
+*            @login_required
+*            def sample_action():
+*                ###
+*                ###
+*                return Response()
+* 수정이 필요할 수 있음
+* 페이지를 넘길 때 유효성 검사를 하려면 어떻게 해야..
+* 토큰이 만료된 경우 처음화면으로 돌아가게 할 수 있나?
+* app.py에 적용 필요함
+"""
+def decode_token(my_token): # 매개 변수로 토큰을 받아옴
+    try:
+        # payload 변수에 jwt토큰을 decode
+        payload = jwt.decode(my_token, SECRET_KEY, algorithms = "HS256")
+
+        # 토큰이 만료된 경우
+        if payload['exp'] < datetime.utcnow():
+            payload = None  
+
+    #부적절한 토큰인 경우 예외 발생 
+    except jwt.InvalidTokenError:
+        payload = None
+    else:
+        return payload
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwagrs):
+        # 토큰을 가져옴
+        # 추후에 가져오는 방법이 바뀔 수 있음
+        # 프론트에서 JWT토큰을 헤더에 넣어 'my_token' 키로 전달
+        my_token = request.headers.get("my_token")
+
+        # 토큰을 가져오면
+        if my_token != None:
+            payload = decode_token(my_token)
+            # 토큰 Decode가 실패하면
+            if payload == None:
+                return False
+        # 토큰 가져오는 것을 실패하면
+        else:
+            return False
+        return f(*args, **kwagrs)
+    return decorated_function
+
+"""
+* 토큰으로부터 user_id 얻기
+* 수정이 필요할 수 있음
+* app.py에 적용 필요함
+"""
+def get_id():
+    try:
+        # 토큰을 가져옴
+        # 추후에 가져오는 방법이 바뀔 수 있음
+        # 프론트에서 JWT토큰을 헤더에 넣어 'my_token' 키로 전달
+        my_token = request.headers.get("my_token")
+
+        #JWT 토큰을 Decode
+        payload = jwt.decode(my_token, SECRET_KEY, algorithms = "HS256")
+
+        # 토큰이 만료된 경우
+        if payload['exp'] < datetime.utcnow():
+            payload = None
+    #부적절한 토큰인 경우 예외 발생 
+    except jwt.InvalidTokenError:
+        payload = None
+
+    try:
+        # Payload가 있으면
+        if payload != None:
+            user_id = payload['user_id']
+            # user_id 리턴
+            return user_id
+        else:
+            return False
+    except Exception as ex:
+        print("***********")
+        print(ex)
+        print("***********")
